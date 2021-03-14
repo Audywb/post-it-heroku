f9a7de0fb58bd2300c959579293920ce4f305ba2
Audywb <woranat.bo.62@ubu.ac.th>
2021-03-15 01:59:31 +0700

    fix readme

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
-# Joon_Cute
-อย่าลืมเชื่อม mongodb 
+# Post it 
+# group 5
+
 เพิ่มไฟล์ config.js ,
+//
+
+module.exports = {
+    MONGODB: 'mongodb+srv://junemon:junemongo1213@cluster0.evsry.mongodb.net/social?retryWrites=true&w=majority',
+    SECRET_KEY: 'some very secret key'
+}
+
+//
 npm i 
-อย่าลืมรัน 
-1.>Joon_Cute>npm start 
-2.>Joon_Cute>client> npm start
+1.>post-it-heroku>
+npm i 
+npm start 
+2.>post-it-heroku>client>
+npm i 
+npm start 
+
+
+อย่าลืมเชื่อม mongodb 
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const {MONGODB} = require('./config');
 const resolvers = require('./graphql/resolvers');
 const typeDefs = require('./graphql/typeDefs');
 const pubsub = new PubSub();
+const PORT = process.env.port || 5000;
 
 const server = new ApolloServer({
     typeDefs,
@@ -15,8 +16,11 @@ const server = new ApolloServer({
 mongoose.connect( MONGODB,{ useNewUrlParser: true} )
 .then(() => {
     console.log('MongoDB Connected');
-    return server.listen({ port: 5000});
+    return server.listen({ port: PORT});
 })
 .then(res => {
     console.log(`server รันอยู่จร้า`);
-});
+})
+.catch(err => {
+    console.error(err)
+})
